76d39166f06b5f4e02fd806b5b3acc274ec3f3b1
d3vkk <donaldkk22@yahoo.com>
2020-07-30 14:40:27 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    /*
+        Create this file with:
+        npx tailwind init
+
+        You can define customizations
+        that override defaults
+    */
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem'
+        }
+    },
+    variants: {},
+    plugins: []
+};
